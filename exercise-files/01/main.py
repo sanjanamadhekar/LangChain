a2f4fc5ca9cbc8838c7395582c4cadf7e9e75b54
Sandra mada <sandramada@sandras-mbp.home>
2024-06-25 10:37:08 +0200

    01_01b

---
+++ exercise-files/01/main.py
@@ -0,0 +1,47 @@
+from dotenv import load_dotenv
+from langchain_openai import OpenAI
+from colorama import Fore
+
+load_dotenv()
+
+
+def generate(text):
+    """ generate text based on the input """
+    pass
+
+
+def start():
+    instructions = (
+        "Type your question and press ENTER. Type 'x' to go back to the MAIN menu.\n"
+    )
+    print(Fore.BLUE + "\n\x1B[3m" + instructions + "\x1B[0m" + Fore.RESET)
+
+    print("MENU")
+    print("====")
+    print("[1]- Ask a question")
+    print("[2]- Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        ask()
+    elif choice == "2":
+        print("Goodbye!")
+        exit()
+    else:
+        print("Invalid choice")
+        start()
+
+
+def ask():
+    while True:
+        user_input = input("Q: ")
+        # Exit
+        if user_input == "x":
+            start()
+        else:
+            response = generate(user_input)
+            print(Fore.BLUE + f"A: " + response + Fore.RESET)
+            print(Fore.WHITE + "\n-------------------------------------------------")
+
+
+if __name__ == "__main__":
+    start()
