4d13c6508c887071dbd341a109cdc6871b5bec9e
Sandra mada <sandramada@sandras-mbp.home>
2024-07-03 13:58:24 +0200

    02_02e

--- exercise-files/02/main.py
+++ exercise-files/02/main.py
@@ -4,7 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+
 # create a vectorstore & embeddings
+vectorstore = FAISS.from_texts(
+    ["harrison worked at kensho"], embedding=OpenAIEmbeddings()
+)
 
 # querying the vectorstore
 
