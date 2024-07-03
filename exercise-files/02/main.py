081587ebe15f8ddbe5cae03d6f70d23aa1ed4994
Sandra mada <sandramada@sandras-mbp.home>
2024-07-03 15:49:34 +0200

    02_04b

--- exercise-files/02/main.py
+++ exercise-files/02/main.py
@@ -1,9 +1,13 @@
 from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings, OpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
+
+model = OpenAI()
 
 template = """Answer the question based only on the following context:
 {context}
@@ -20,9 +24,27 @@ vectorstore = FAISS.from_texts(
 # querying the vectorstore
 query = "Where did Harrison work?"
 docs = vectorstore.similarity_search(query, top_k=1)
-print(docs[0].page_content)
+# print(docs[0].page_content)
 
 
 # querying as retriever
+retriever = vectorstore.as_retriever()
+docs = retriever.invoke(query, top_k=1)
+# print(docs[0].page_content)
+
+
+retrieval_chain = (
+    {
+        "context": retriever,
+        "question": RunnablePassthrough(),
+    }
+    | prompt
+    | model
+    | StrOutputParser()
+)
+
+response = retrieval_chain.invoke(query)
+print(response)
+
 
 
