6de3cc39234efc128d50a2f12bb80ecd623acfe8
Sandra mada <sandramada@sandras-mbp.home>
2024-07-05 18:07:09 +0200

    04_02e

--- exercise-files/04/app.py
+++ exercise-files/04/app.py
@@ -16,14 +16,35 @@ chat_placeholder = st.empty()
 
 def init_chat_history():
     """Initialize chat history with a system message."""
-    pass
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = []
+        st.session_state["messages"].append(
+            {"role": "system", "message": "You are a helpful assistant. Ask me anything!"}
+        )
 
 
 def start_chat():
     """Start the chatbot conversation."""
     # Display chat messages from history on app rerun
-    pass
+    with chat_placeholder.container():
+        for message in session_state.messages:
+            if message["role"] == "system":
+                with st.chat_message(message["role"]):
+                    st.markdown(message["content"])
 
+    if prompt := st.chat_input("What is up?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+
+        with st.chat_message("assistant"):
+            st.markdown("")
+        
+        st.session_state.messages.append({"role": "user", "content": ""})
+
+            
 
 if __name__ == "__main__":
     init_chat_history()
