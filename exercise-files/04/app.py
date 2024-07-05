a6e92939444f6b8cf958eaec3bfe304c9329fee4
Sandra mada <sandramada@sandras-mbp.home>
2024-07-05 17:02:17 +0200

    04_01b

---
+++ exercise-files/04/app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import openai
+import os
+from dotenv import load_dotenv
+from main import query
+
+load_dotenv()
+
+API_KEY = os.getenv("OPENAI_API_KEY")
+openai.api_key = API_KEY
+MODEL_ENGINE = "gpt-3.5-turbo"
+
+st.title("🤖 Chatbot App")
+chat_placeholder = st.empty()
+
+
+def init_chat_history():
+    """Initialize chat history with a system message."""
+    pass
+
+
+def start_chat():
+    """Start the chatbot conversation."""
+    # Display chat messages from history on app rerun
+    pass
+
+
+if __name__ == "__main__":
+    init_chat_history()
+    start_chat()
