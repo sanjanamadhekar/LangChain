bd22ad03c41ebd0e0600004a06dab70416de1ad7
Sandra mada <sandramada@sandras-mbp.home>
2024-07-05 18:12:52 +0200

    04_03b

--- exercise-files/04/app.py
+++ exercise-files/04/app.py
@@ -18,33 +18,38 @@ def init_chat_history():
     """Initialize chat history with a system message."""
     if "messages" not in st.session_state:
         st.session_state["messages"] = []
-        st.session_state["messages"].append(
-            {"role": "system", "message": "You are a helpful assistant. Ask me anything!"}
-        )
+        st.session_state.messages = [
+            {"role": "system", "content": "You are a helpful assistant."}
+        ]
 
 
 def start_chat():
     """Start the chatbot conversation."""
     # Display chat messages from history on app rerun
     with chat_placeholder.container():
-        for message in session_state.messages:
-            if message["role"] == "system":
+        for message in st.session_state.messages:
+            if message["role"] != "system":
                 with st.chat_message(message["role"]):
                     st.markdown(message["content"])
 
+    # Accept user input
     if prompt := st.chat_input("What is up?"):
+        # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
 
+        # Display user message in chat message container
         with st.chat_message("user"):
             st.markdown(prompt)
 
-
-        with st.chat_message("assistant"):
-            st.markdown("")
+        # Generate response from Chat models
         
-        st.session_state.messages.append({"role": "user", "content": ""})
 
-            
+        # message_placeholder.markdown(response)
+        with st.chat_message("assistant"):
+            st.markdown(response)
+        # Add assistant's response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
 
 if __name__ == "__main__":
     init_chat_history()
