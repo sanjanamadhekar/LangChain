faa3fbb308280db7d091ff521635e1d9f3ae1412
Sandra mada <sandramada@sandras-mbp.home>
2024-07-08 11:37:36 +0200

    05_02e

--- exercise-files/05/main.py
+++ exercise-files/05/main.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders import TextLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
-from langchain_text_splitters import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter, RecursiveCharacterTextSplitter
 from langchain_core.output_parsers.openai_tools import PydanticToolsParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
@@ -20,5 +20,18 @@ llm = ChatOpenAI(temperature=0)
 # Prompt with Query analysis
 
 # Create Index & Connect to datasource
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
+texts = ["Harrison worked at Kensho"]
+vectorstore = Chroma.from_texts(texts, embeddings, collection_name="harrison")
+retriever_harrison = vectorstore.as_retriever(search_kwargs={"k": 1})
+
+texts = ["Ankush worked at Facebook"]
+vectorstore = Chroma.from_texts(texts, embeddings, collection_name="ankush")
+retriever_ankush = vectorstore.as_retriever(search_kwargs={"k": 1})
+
+docs = vectorstore.similarity_search("who worked at Facebook?")
+print(docs[0].page_content)
+
 
 # Retrieval with Query analysis
